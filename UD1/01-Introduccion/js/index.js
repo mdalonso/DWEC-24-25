e7d1d29d13da0978da5544c8d5069a9a222d2bd7
mdalonso <38704285+mdalonso@users.noreply.github.com>
2024-09-14 19:05:33 +0200

    Add files via upload

---
+++ UD1/01-Introduccion/js/index.js
@@ -0,0 +1,25 @@
+
+//Uso de comentarios en un archivo
+
+//comentario de una línea
+
+/* comentarios
+de varias
+lineas */
+
+
+//Mostrar un mensaje por consola. En Chrome la consola se muestra pulsando F12
+console.log("Mi primer script en JavaScript"); 
+
+//Muestra un mensaje en una ventana independiente
+alert("Buenos días");
+
+//Pide al usuario que introduzca un número en una ventana independiente. 
+//Prompt devuelve el valor introducido por el usuario. 
+//Si el usuario no introduce nada, devuelve null.
+//La siguiente línea muestra por consola el número que introduce el usuario.
+console.log(prompt("Introduzca número"))
+
+//confirm devuelve true si el usuario pulsa Confirmar y False si pulsa Cancelar
+console.log(confirm("¿Desea continuar?"))
+
