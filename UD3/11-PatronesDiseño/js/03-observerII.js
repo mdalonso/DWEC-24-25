aed3c976b5f02df3271894b8065b6b5abb6e20aa
mdalonso <38704285+mdalonso@users.noreply.github.com>
2025-11-11 20:51:32 +0100

    Update and rename 06-observerII.js to 03-observerII.js

--- UD3/11-PatronesDiseño/js/03-observerII.js
+++ UD3/11-PatronesDiseño/js/03-observerII.js
@@ -1,4 +1,5 @@
 "use strict";
+//SISTEMA QUE RECIBE DATOS Y SI ESTÁN FUERA DE RANGO EMITE UNA ALERTA.
 class FuenteDeDatos {
     constructor() {
       this.observadores = [];
@@ -9,17 +10,18 @@ class FuenteDeDatos {
     }
   
     nuevaLectura(dato) {
+        //Se recibe el nuevo dato. Aquí estaría el código de procesamiento que haría la lectura por ejemplo de unos sensores
       console.log(`Nueva lectura de datos: ${dato}`);
       this.notificar(dato);
     }
   
     notificar(dato) {
-      this.observadores.forEach(observador => observador.actualizar(dato));
+      this.observadores.forEach(observador => observador.enviarAlerta(dato));
     }
   }
   
   class SistemaDeAlertas {
-    actualizar(dato) {
+    enviarAlerta(dato) {
       if (dato > 100) {
         console.log("¡Alerta! Dato fuera de rango:", dato);
       }
@@ -34,4 +36,5 @@ class FuenteDeDatos {
   fuenteDeDatos.nuevaLectura(150);
   // "Nueva lectura de datos: 150"
   // "¡Alerta! Dato fuera de rango: 150"
+
   
